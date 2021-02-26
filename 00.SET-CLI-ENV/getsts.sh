d64bdd0d602b758723e3466afce3bcc43ac048b9
kukubabo <joonsik.eom@gmail.com>
2021-02-26 16:00:06 +0900

    2021-02-26 update

---
+++ 00.SET-CLI-ENV/getsts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+### SET Account Info. ##############
+MASTER_ID=123456789012
+IAMUSR_ID=iamuser
+SVC_ALIAS=infra
+####################################
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 123456"
+    exit 1
+fi
+
+AWS_DIR=~/.aws
+
+sed -i '4,7d' ${AWS_DIR}/credentials
+
+aws sts get-session-token --serial-number arn:aws:iam::${MASTER_ID}:mfa/${IAMUSR_ID} --profile default --token-code $1 > ${AWS_DIR}/getsts.rst
+
+if [ $? -eq 0 ]; then
+    NEW_AKI=`cat ${AWS_DIR}/getsts.rst | jq -r '.Credentials.AccessKeyId'`
+    NEW_SAK=`cat ${AWS_DIR}/getsts.rst | jq -r '.Credentials.SecretAccessKey'`
+    NEW_ST=` cat ${AWS_DIR}/getsts.rst | jq -r '.Credentials.SessionToken'`
+
+    echo "[mfa]"                              >> ${AWS_DIR}/credentials
+    echo "aws_access_key_id = ${NEW_AKI}"     >> ${AWS_DIR}/credentials
+    echo "aws_secret_access_key = ${NEW_SAK}" >> ${AWS_DIR}/credentials
+    echo "aws_session_token = ${NEW_ST}"      >> ${AWS_DIR}/credentials
+
+    cp -p ${AWS_DIR}/credentials ${AWS_DIR}/credentials.${SVC_ALIAS}
+else
+    echo "[ERROR] get-session-token error!!"
+fi
+
+rm ${AWS_DIR}/getsts.rst
